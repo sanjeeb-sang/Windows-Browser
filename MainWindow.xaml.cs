c06fbfca1f69d963fbcc7f702d89c01e6c5d9e42
sanjeeb9853 <sanjeeb.github@gmail.com>
2018-06-29 18:38:37 +0545

    Initial Commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace WebBrowser_Version_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : NavigationWindow
+    {
+
+        // Instance variables to be used in this class
+        // End of Instance Variables
+        
+
+        // MainWindow Constructor
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void DockPanel_SourceUpdated(object sender, DataTransferEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Views/IndexPage.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using System.Windows.Markup;
+using mshtml;
+
+
+namespace WebBrowser_Version_2.Views
+{
+    /// <summary>
+    /// Interaction logic for IndexPage.xaml
+    /// </summary>
+    public partial class IndexPage : Page
+    {
+        String _HomeUrl = "https://www.google.com";
+ 
+
+
+        public IndexPage()
+        {
+            InitializeComponent();
+        }
+
+        // The Selected Value in the ComboBox has Been Changed
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        // The mWebBrowser has been Loaded
+        private void WebBrowser_Loaded(object sender, RoutedEventArgs e)
+        {
+            mWebBrowser.Navigate(_HomeUrl);
+
+        }
+
+        // The mWebBrowser has Completely Loaded a page
+        private void WebBrowser_LoadCompleted(object sender, NavigationEventArgs e) {
+
+            setCustomStyleSheet(" body {zoom: 120%; }");
+        }
+
+       
+
+        // For the BackButton
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (mWebBrowser.CanGoBack)
+            {
+                mWebBrowser.GoBack();
+
+            }
+        }
+
+        // For the HomeButton
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigateToHomePage();
+        }
+
+        // For the UpButton
+        private void UpButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (mWebBrowser.CanGoForward)
+            {
+                mWebBrowser.GoForward();
+            }
+        }
+        
+
+
+
+        // Helper methods
+        private void NavigateToHomePage()
+        {
+            mWebBrowser.Navigate(_HomeUrl);
+        }
+
+        private HTMLDocument getHtmlDocument(WebBrowser mWebBrowser)
+        {
+            HTMLDocument _htmlDocument = mWebBrowser.Document as HTMLDocument;
+
+            return _htmlDocument;
+
+        }
+        private void setCustomStyleSheet(String styleString)
+        {
+            IHTMLStyleSheet customCSS = getHtmlDocument(mWebBrowser).createStyleSheet();
+
+            customCSS.cssText = styleString;
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            mWebBrowser.Navigate("https://www.google.com/#q=" + mSearchTextBox.Text.ToString());
+        }
+
+        private void mSearchTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            mSearchTextBox.Text = "";
+        }
+
+        private void mSearchTextBox_OnMouseDirectlyOver(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            mSearchTextBox.Text = "";
+        }
+
+        private void mSearchTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
